171b1afc1676a0dcc241d8de29def84d1f4820c2
Gabriel <sharina@sharinas-air.myfiosgateway.com>
2021-07-28 11:07:15 -0400

    added server and script

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 //old syntaxt
 // consdt express =require("express")
 //type:module
-import express from "express"
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 4567;
+
+//give access to req.body
+
+app.use(express.json());
+app.use(cors);
+//change after deployment and we know everything is worng "tiny or small"
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("<h1>Hello im here</h1>"))
+
+app.listen(PORT, console.log(`Connected to port: ${PORT}`))
